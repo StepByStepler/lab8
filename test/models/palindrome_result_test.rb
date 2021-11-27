b99ccd2c4aa36b0c29b28af30299871841d520f0
Grigory Panko <gpanko@bk.ru>
2021-11-27 21:22:32 +0300

    Implement PalindromeResult tests and clean its fixtures

--- test/models/palindrome_result_test.rb
+++ test/models/palindrome_result_test.rb
@@ -1,7 +1,10 @@
-require "test_helper"
+require 'test_helper'
 
 class PalindromeResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should check uniqueness of objects by input' do
+    res1 = PalindromeResult.new max: 25, result: [0, 1, 2, 3, 11, 12]
+    assert res1.save
+    res2 = PalindromeResult.new max: 25, result: [0, 1, 2, 3, 11, 12, 150]
+    assert_not res2.save
+  end
 end
