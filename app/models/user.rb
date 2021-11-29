2f8047032004ac646586bf6e64ff96d50782353e
gpank <gpanko@bk.ru>
2021-11-29 22:13:17 +0300

    Add name uniqueness validation in user, fix something with dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_uniqueness_of :name
 
-  # def has_password?(submitted_password)
-  #   p "password = #{password_digest}"
-  #   p "hash = #{BCrypt::Password.create(submitted_password)}"
-  #   p "new = #{BCrypt::Password.new(submitted_password)}"
-  #   p "result = #{BCrypt::Password.new(submitted_password) == password_digest}"
-  #   BCrypt::Password.new(submitted_password) == password_digest
-  # end
-  #
   def self.authenticate(name, submitted_password)
     user = find_by_name(name)
     return nil if user.nil?
